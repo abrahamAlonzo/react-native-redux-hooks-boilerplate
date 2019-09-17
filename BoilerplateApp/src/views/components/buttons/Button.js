3e598ef0bc57382e7c3214b6142048faf76d2fef
abrahamAlonzo <abraham.alonzo314@gmail.com>
2019-09-17 02:07:14 -0500

    some flat files for init project added

---
+++ BoilerplateApp/src/views/components/buttons/Button.js
@@ -0,0 +1,20 @@
+import React, { useState, useContext } from 'react';
+import {
+    View,
+    Text,
+    StyleSheet,
+} from 'react-native';
+
+function Button() {
+    return (
+        <View style={styles.container} />
+    );
+}
+
+const styles = {
+    container: {
+        flex: 1
+    }
+};
+
+export default Button;
---
+++ BoilerplateApp/src/views/home/Home.js
@@ -0,0 +1,20 @@
+import React, { useState, useContext } from 'react';
+import {
+    View,
+    Text,
+    StyleSheet,
+} from 'react-native';
+
+function Home() {
+    return (
+        <View style={styles.container} />
+    );
+}
+
+const styles = {
+    container: {
+        flex: 1
+    }
+};
+
+export default Home;
